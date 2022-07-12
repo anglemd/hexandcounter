42e399258e79b56f5211781beeb55eeb159a423d
AngleMD <david.gonzalez@anglemd.com>
2022-07-11 21:24:16 -0700

    hashLocationStrategy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client';
+  title = 'HnC';
+  public userName$: Observable<string>;
+
+  constructor(private authService: AuthService) {
+    this.userName$ = authService.userNameSubject$;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
